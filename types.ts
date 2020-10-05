8b891bf446619a801c9dbdc5cef32b68472774bb
antonwis <anton.wiskari@metropolia.fi>
2020-10-05 03:30:54 +0300

    välicommit kys

--- types.ts
+++ types.ts
@@ -1,9 +1,18 @@
 export interface Employee {
-    firstName: string;
-    lastName: string;
-    address: string;
-    phone: {
-        home: string;
-        work: string | null;
-    }
+  firstName: string;
+  lastName: string;
+  address: string;
+  phone: {
+    home: string;
+    work: string | null;
+  };
 }
+
+export interface Band {
+    nameLink: string;
+    country: string;
+    genre: string;
+    status: string;
+  };
+
+
